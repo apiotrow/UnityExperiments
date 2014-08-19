721d4eb5d2eb0c65388da7b9185515c6867f5168
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2014-08-19 01:58:56 -0700

    gravity and balance

--- Assets/PlanetController.cs
+++ Assets/PlanetController.cs
@@ -3,25 +3,136 @@ using System.Collections;
 
 public class PlanetController : MonoBehaviour {
 
-//	public Planet planet1;
-//	public Planet planet2;
-//	public Planet planet3;
-//	public Planet planet4;
-//	public Planet planet5;
-//	public Planet planet6;
-//	public Planet planet7;
-
 	public Planet[] planets;
+
+	int numPlanets = 4;
+	TrailRenderer trail;
+	float massMax = 5f;
+	float massMin;
+	MouseOrbit morb;
+
+	void OnGUI ()
+	{
+
+		if (GUI.Button (new Rect (Screen.width - 200, 0, 100, 20), "R to restart")) {
+			Application.LoadLevel (0);  
+		}
+
+
+		int thisbox = 0;
+		string planetsstr = "# planets";
+		GUI.Box (new Rect (Screen.width - 100, thisbox, 100, 25), planetsstr);
+		if (GUI.Button (new Rect (Screen.width - 50, thisbox + 20, 50, 20), "+")) {
+			numPlanets += 1;
+			PlayerPrefs.SetInt("numplan", numPlanets);
+		}
+		if (GUI.Button (new Rect (Screen.width - 100, thisbox+ 20, 50, 20), "-")) {
+			numPlanets -= 1;
+			if(numPlanets < 2 ) numPlanets = 2;
+			PlayerPrefs.SetInt("numplan", numPlanets);
+		}
+		string strnumPlanets = "N:" + (Mathf.Floor (numPlanets)).ToString ();
+		GUI.Box (new Rect (Screen.width - 100, thisbox + 40, 100, 20), strnumPlanets);
+
+
+		thisbox = thisbox + 90;
+		planetsstr = "Trails";
+		GUI.Box (new Rect (Screen.width - 100, thisbox, 100, 25), planetsstr);
+		if (GUI.Button (new Rect (Screen.width - 50, thisbox + 20, 50, 20), "on")) {
+			PlayerPrefs.SetInt("trails", 1);
+			trail.enabled = true;
+		}
+		if (GUI.Button (new Rect (Screen.width - 100, thisbox+ 20, 50, 20), "off")) {
+			PlayerPrefs.SetInt("trails", 0);
+			trail.enabled = false;
+		}
+		strnumPlanets = "Trails: " + trail.enabled;
+		GUI.Box (new Rect (Screen.width - 100, thisbox + 40, 100, 20), strnumPlanets);
+
+
+		thisbox = thisbox + 100;
+		planetsstr = "Mass Range";
+		GUI.Box (new Rect (Screen.width - 100, thisbox, 100, 25), planetsstr);
+		if (GUI.Button (new Rect (Screen.width - 25, thisbox + 20, 25, 20), "+")) {
+			massMax += 1;
+			PlayerPrefs.SetFloat("massMax", massMax);
+		}
+		if (GUI.Button (new Rect (Screen.width - 50, thisbox+ 20, 25, 20), "-")) {
+			massMax -= 1;
+			if (massMax <= (massMin + 1)) massMax = (massMin + 1);
+			PlayerPrefs.SetFloat("massMax", massMax);
+		}
+		strnumPlanets = "" + massMax;
+		GUI.Box (new Rect (Screen.width - 50, thisbox + 40, 50, 20), strnumPlanets);
+
+		if (GUI.Button (new Rect (Screen.width - 75, thisbox + 20, 25, 20), "+")) {
+			massMin += 1;
+			if (massMin >= (massMax - 1)) massMin = (massMax - 1);
+			PlayerPrefs.SetFloat("massMin", massMin);
+		}
+		if (GUI.Button (new Rect (Screen.width - 100, thisbox+ 20, 25, 20), "-")) {
+			massMin -= 1;
+			if(massMin < 1) massMin = 1;
+			PlayerPrefs.SetFloat("massMin", massMin);
+		}
+		strnumPlanets = "" + massMin;
+		GUI.Box (new Rect (Screen.width - 100, thisbox + 40, 50, 20), strnumPlanets);
+	}
 
 
 	void Start () {
-		planets = new Planet[7];
-		//		planet1 = GameObject.Find ("Planet1").GetComponent ("Planet") as Planet;
-		//		planet2 = GameObject.Find ("Planet2").GetComponent ("Planet") as Planet;
-		//		planet3 = GameObject.Find ("Planet3").GetComponent ("Planet") as Planet;
 
-		for(int i = 0; i < planets.Length; i++){
-			planets[i] = GameObject.Find ("Planet" + (i + 1).ToString()).GetComponent ("Planet") as Planet;
+		morb = Camera.main.GetComponent<MouseOrbit>();
+//		MouseOrbit.distance = 200f;
+
+		numPlanets = PlayerPrefs.GetInt ("numplan");
+		//planets = new Planet[Random.Range (3, 15)];
+		planets = new Planet[numPlanets];
+		planets[0] = GameObject.Find ("Planet1").GetComponent ("Planet") as Planet;
+		trail = planets[0].GetComponent<TrailRenderer>();
+
+		if (PlayerPrefs.GetInt ("trails") == 0)
+			trail.enabled = false;
+		else if(PlayerPrefs.GetInt ("trails") == 1)
+			trail.enabled = true;
+
+		planets[1] = GameObject.Find ("Planet3").GetComponent ("Planet") as Planet;
+		planets [1].renderer.material.color = Color.yellow;
+
+		float maxrange = 200f;
+
+		for(int i = 2; i < planets.Length; i++){
+			Vector3 randLoc = new Vector3(Random.Range (-maxrange,maxrange), Random.Range (-maxrange,maxrange), Random.Range (-maxrange,maxrange));
+
+			//planets[i] = GameObject.Find ("Planet" + (i + 1).ToString()).GetComponent ("Planet") as Planet;
+			Planet newplanet = Instantiate (planets[0], randLoc, transform.rotation) as Planet;
+			planets[i] = newplanet;
+
+			//planets[i].rigidbody.mass = Random.Range (1f, 2f);
+			planets[i].rigidbody.mass = Random.Range (PlayerPrefs.GetFloat ("massMin"), PlayerPrefs.GetFloat ("massMax"));
+			massMax = PlayerPrefs.GetFloat ("massMax");
+			massMin = PlayerPrefs.GetFloat ("massMin");
+
+			planets[i].renderer.material.color = new Color(Random.Range (0f,1f),Random.Range (0f,1f),Random.Range (0f,1f));
+
+			float randScale = Random.Range (1f, 6f);
+			planets[i].transform.localScale += new Vector3(randScale,randScale,randScale);
+
+			float rand = Random.Range (0f,1f);
+			Vector3 dir;
+			
+			if(rand > 0f && rand < 0.25f)
+				dir = Vector3.left;
+			else if(rand > 0.25f && rand < 0.5f)
+				dir = Vector3.right;
+			else if(rand > 0.5f && rand < 0.75f)
+				dir = Vector3.forward;
+			else if(rand > 0.75f && rand < 1f)
+				dir = Vector3.back;
+			else
+				dir = Vector3.back;
+			
+			planets[i].rigidbody.AddForce (dir * 900);
 		}
 
 
@@ -37,27 +148,30 @@ public class PlanetController : MonoBehaviour {
 //		planets[2].rigidbody.AddForce (Vector3.left * 8900000);
 
 
-		for(int i = 0; i < planets.Length; i++){
-			float rand = Random.Range (0f,1f);
-			Vector3 dir;
-
-			if(rand > 0f && rand < 0.25f)
-				dir = Vector3.left;
-			else if(rand > 0.25f && rand < 0.5f)
-				dir = Vector3.right;
-			else if(rand > 0.5f && rand < 0.75f)
-				dir = Vector3.forward;
-			else if(rand > 0.75f && rand < 1f)
-				dir = Vector3.back;
-			else
-				dir = Vector3.back;
-
-			planets[i].rigidbody.AddForce (dir * 8);
-		}
+//		for(int i = 0; i < planets.Length; i++){
+//			float rand = Random.Range (0f,1f);
+//			Vector3 dir;
+//
+//			if(rand > 0f && rand < 0.25f)
+//				dir = Vector3.left;
+//			else if(rand > 0.25f && rand < 0.5f)
+//				dir = Vector3.right;
+//			else if(rand > 0.5f && rand < 0.75f)
+//				dir = Vector3.forward;
+//			else if(rand > 0.75f && rand < 1f)
+//				dir = Vector3.back;
+//			else
+//				dir = Vector3.back;
+//
+//			planets[i].rigidbody.AddForce (dir * 8);
+//		}
 	}
 	
 
 	void FixedUpdate () {
+		if (Input.GetKey (KeyCode.R)) {
+			Application.LoadLevel (0); 
+		}
 		//global stuff
 //		Vector3 planet1ToPlanet2 = planet2.transform.position - planet1.transform.position;
 //		planet1.rigidbody.AddForce (planet1ToPlanet2 / (planet2.rigidbody.mass / 500000));
@@ -72,6 +186,13 @@ public class PlanetController : MonoBehaviour {
 		                        //      / (planets[1].rigidbody.mass / 50000));
 
 		for(int i = 0; i < planets.Length; i++){
+			trail = planets[i].GetComponent<TrailRenderer>();
+			
+			if (PlayerPrefs.GetInt ("trails") == 0)
+				trail.enabled = false;
+			else if(PlayerPrefs.GetInt ("trails") == 1)
+				trail.enabled = true;
+
 			for(int j = 0; j < planets.Length; j++){
 					if (i != j)
 					planets[i].rigidbody.AddForce((planets[j].transform.position - planets[i].transform.position)
---
+++ Assets/Plate.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Plate : MonoBehaviour
+{
+	Vector3 platePos;
+	Vector3 ballPos;
+	float dist;
+	public GameObject ball;
+	public Quaternion thingy;
+
+	void Start ()
+	{
+		ball = GameObject.Find ("Ball");
+		//ball.renderer.material.color = Color.red;
+	}
+
+	void FixedUpdate ()
+	{
+		platePos = transform.position;
+		ballPos = ball.transform.position;
+
+		float xdiff = platePos.x - ballPos.x;
+		float zdiff = platePos.z - ballPos.z;
+		dist = Vector3.Distance (ballPos, platePos);
+		float xrot = transform.rotation.eulerAngles.x;
+		float zrot = transform.rotation.eulerAngles.z;
+		//dist *= 1f;
+
+		Vector3 start = new Vector3 (transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z);
+
+		if (xdiff < 0) {
+	
+//			if (zrot >= 70 && zrot <= 180) {
+//				Vector3 end = new Vector3 (transform.eulerAngles.x, transform.eulerAngles.y, start.z - 1);
+//				transform.eulerAngles = end;
+//			} else {
+				transform.RotateAround (transform.position, Vector3.forward, Time.deltaTime * dist);
+//			}
+					
+		} else {
+	
+//			if (zrot >= 180 && zrot <= 270) {
+//				Vector3 end = new Vector3 (transform.eulerAngles.x, transform.eulerAngles.y, start.z + 1);
+//				transform.eulerAngles = end;
+//			} else {
+				transform.RotateAround (transform.position, Vector3.back, Time.deltaTime * dist);
+//			}
+		}
+
+		if (zdiff < 0) {
+
+//			if (xrot >= 70 && xrot <= 180) {
+//				Vector3 end = new Vector3 (start.x - 1, transform.eulerAngles.y, transform.eulerAngles.z);
+//				transform.eulerAngles = end;
+//			}else{
+				transform.RotateAround (transform.position, Vector3.left, Time.deltaTime * dist);
+//			}
+		} else {
+
+//			if (xrot >= 180 && xrot <= 270) {
+//				Vector3 end = new Vector3 (start.x + 1, transform.eulerAngles.y, transform.eulerAngles.z);
+//				transform.eulerAngles = end;
+//			}else{
+				transform.RotateAround (transform.position, Vector3.right, Time.deltaTime * dist);
+//			}
+		}
+
+		Vector3 v = ball.rigidbody.velocity;
+		v.y = -10f;
+		//ball.rigidbody.velocity = v;
+
+
+		//Debug.Log (transform.rotation.eulerAngles.z);
+
+
+	}
+}
