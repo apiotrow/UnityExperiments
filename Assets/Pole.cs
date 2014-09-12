886a02912a2d601f8a8a558b8854ba255538c54c
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2014-09-11 21:27:09 -0700

    Removed motion blue. Added objects culled in only 1 eye.

---
+++ Assets/Pole.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pole : MonoBehaviour {
+	public Vector3 around;
+	public Vector3 around2;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround (transform.position, around, Time.deltaTime * 40);
+		transform.RotateAround (transform.position, around2, Time.deltaTime * 10);
+	}
+}
