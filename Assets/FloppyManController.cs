98e6936e7edb69ec7d8e3f07e8b2900b64c63c8d
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2014-09-27 00:01:51 -0700

    changed drag, added restart button

---
+++ Assets/FloppyManController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloppyManController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.R)) {
+			Application.LoadLevel (0); 
+		}
+	}
+}
